98c1808cb1f0d2c8b974d4d48e24a053c2225b2b
Dame36 <damegthaplm>
2022-03-11 18:09:29 +0700

    update pack image

--- img-pre/pack_img3.py
+++ img-pre/pack_img3.py
@@ -67,145 +67,61 @@ path_p6_l2 = 'abRSOnLIR'
 path_p6_l3 = 'nRSOabLIR'
 path_p6_l4 = 'abRSOabLIR'
 
-numlabel1 = 0
-numlabel2 = 0
-numlabel3 = 0
-numlabel4 = 0
-
 
 #number of each label and append to list
 
-for file in natsorted(os.listdir(path+path_p1_l1)):
-    numlabel1 += 1
-    box1_l1.append(file)
-print(numlabel1)
-print(box1_l1)
+box1_l1 = [file for file in natsorted(os.listdir(path+path_p1_l1))]
 
-for file in natsorted(os.listdir(path+path_p1_l2)):
-    numlabel2 += 1
-    box1_l2.append(file)
-print(numlabel2)
-print(box1_l2)
+box1_l2 = [file for file in natsorted(os.listdir(path+path_p1_l2))]
 
-for file in natsorted(os.listdir(path+path_p1_l3)):
-    numlabel3 += 1
-    box1_l3.append(file)
-print(numlabel3)
-print(box1_l3)
+box1_l3 = [file for file in natsorted(os.listdir(path+path_p1_l3))]
 
-for file in natsorted(os.listdir(path+path_p1_l4)):
-    numlabel4 += 1
-    box1_l4.append(file)
-print(numlabel4)
-print(box1_l4)
+box1_l4 = [file for file in natsorted(os.listdir(path+path_p1_l4))]
 
 
+box2_l1 = [file for file in natsorted(os.listdir(path+path_p2_l1))]
 
-for file in natsorted(os.listdir(path+path_p2_l1)):
-    box2_l1.append(file)
-print(numlabel1)
-print(box2_l1)
+box2_l2 = [file for file in natsorted(os.listdir(path+path_p2_l2))]
 
-for file in natsorted(os.listdir(path+path_p2_l2)):
-    box2_l2.append(file)
-print(numlabel2)
-print(box2_l2)
+box2_l3 = [file for file in natsorted(os.listdir(path+path_p2_l3))]
 
-for file in natsorted(os.listdir(path+path_p2_l3)):
-    box2_l3.append(file)
-print(numlabel3)
-print(box2_l3)
-
-for file in natsorted(os.listdir(path+path_p2_l4)):
-    box2_l4.append(file)
-print(numlabel4)
-print(box2_l4)
+box2_l4 = [file for file in natsorted(os.listdir(path+path_p2_l4))]
 
 
+box3_l1 = [file for file in natsorted(os.listdir(path+path_p3_l1))]
 
-for file in natsorted(os.listdir(path+path_p3_l1)):
-    box3_l1.append(file)
-print(numlabel1)
-print(box3_l1)
+box3_l2 = [file for file in natsorted(os.listdir(path+path_p3_l2))]
 
-for file in natsorted(os.listdir(path+path_p3_l2)):
-    box3_l2.append(file)
-print(numlabel2)
-print(box3_l2)
+box3_l3 = [file for file in natsorted(os.listdir(path+path_p3_l3))]
 
-for file in natsorted(os.listdir(path+path_p3_l3)):
-    box3_l3.append(file)
-print(numlabel3)
-print(box3_l3)
-
-for file in natsorted(os.listdir(path+path_p3_l4)):
-    box3_l4.append(file)
-print(numlabel4)
-print(box3_l4)
+box3_l4 = [file for file in natsorted(os.listdir(path+path_p3_l4))]
 
 
+box4_l1 = [file for file in natsorted(os.listdir(path+path_p4_l1))]
 
-for file in natsorted(os.listdir(path+path_p4_l1)):
-    box4_l1.append(file)
-print(numlabel1)
-print(box4_l1)
+box4_l2 = [file for file in natsorted(os.listdir(path+path_p4_l2))]
 
-for file in natsorted(os.listdir(path+path_p4_l2)):
-    box4_l2.append(file)
-print(numlabel2)
-print(box4_l2)
+box4_l3 = [file for file in natsorted(os.listdir(path+path_p4_l3))]
 
-for file in natsorted(os.listdir(path+path_p4_l3)):
-    box4_l3.append(file)
-print(numlabel3)
-print(box4_l3)
-
-for file in natsorted(os.listdir(path+path_p4_l4)):
-    box4_l4.append(file)
-print(numlabel4)
-print(box4_l4)
+box4_l4 = [file for file in natsorted(os.listdir(path+path_p4_l4))]
 
 
-for file in natsorted(os.listdir(path+path_p5_l1)):
-    box5_l1.append(file)
-print(numlabel1)
-print(box5_l1)
+box5_l1 = [file for file in natsorted(os.listdir(path+path_p5_l1))]
 
-for file in natsorted(os.listdir(path+path_p5_l2)):
-    box5_l2.append(file)
-print(numlabel2)
-print(box5_l2)
+box5_l2 = [file for file in natsorted(os.listdir(path+path_p5_l2))]
 
-for file in natsorted(os.listdir(path+path_p5_l3)):
-    box5_l3.append(file)
-print(numlabel3)
-print(box5_l3)
+box5_l3 = [file for file in natsorted(os.listdir(path+path_p5_l3))]
 
-for file in natsorted(os.listdir(path+path_p5_l4)):
-    box5_l4.append(file)
-print(numlabel4)
-print(box5_l4)
+box5_l4 = [file for file in natsorted(os.listdir(path+path_p5_l4))]
 
 
-for file in natsorted(os.listdir(path+path_p6_l1)):
-    box6_l1.append(file)
-print(numlabel1)
-print(box6_l1)
+box6_l1 = [file for file in natsorted(os.listdir(path+path_p6_l1))]
 
-for file in natsorted(os.listdir(path+path_p6_l2)):
-    box6_l2.append(file)
-print(numlabel2)
-print(box6_l2)
+box6_l2 = [file for file in natsorted(os.listdir(path+path_p6_l2))]
 
-for file in natsorted(os.listdir(path+path_p6_l3)):
-    box6_l3.append(file)
-print(numlabel3)
-print(box6_l3)
+box6_l3 = [file for file in natsorted(os.listdir(path+path_p6_l3))]
 
-for file in natsorted(os.listdir(path+path_p6_l4)):
-    box6_l4.append(file)
-print(numlabel4)
-print(box6_l4)
+box6_l4 = [file for file in natsorted(os.listdir(path+path_p6_l4))]
 
 
 
@@ -223,7 +139,7 @@ os.makedirs(newdir4, exist_ok=True)
 
 
 #for label1(normal)
-for i in range(numlabel1):
+for i in range(len(box1_l1)):
     img1 = cv2.imread(path+path_p1_l1 + '/' + box1_l1[i])
     img2 = cv2.imread(path+path_p2_l1 + '/' + box2_l1[i])
     img3 = cv2.imread(path+path_p3_l1 + '/' + box3_l1[i])
@@ -237,11 +153,12 @@ for i in range(numlabel1):
 
     ver = np.vstack((row1, row2, row3))
     cv2.imwrite(newdir1 + 'normal.' + str(i+1) + '.jpg', ver)
-    cv2.imshow('ver',ver)
+    # print('normal.' + str(i+1) + '.jpg')
+    # cv2.imshow('ver',ver)
     cv2.waitKey(0)
 
 #for label2(abRnL)
-for i in range(numlabel2):
+for i in range(len(box1_l2)):
     img1 = cv2.imread(path+path_p1_l2 + '/' + box1_l2[i])
     img2 = cv2.imread(path+path_p2_l2 + '/' + box2_l2[i])
     img3 = cv2.imread(path+path_p3_l2 + '/' + box3_l2[i])
@@ -258,7 +175,7 @@ for i in range(numlabel2):
     cv2.waitKey(0)
 
 #for label3(nRabL)
-for i in range(numlabel3):
+for i in range(len(box1_l3)):
     img1 = cv2.imread(path+path_p1_l3 + '/' + box1_l3[i])
     img2 = cv2.imread(path+path_p2_l3 + '/' + box2_l3[i])
     img3 = cv2.imread(path+path_p3_l3 + '/' + box3_l3[i])
@@ -275,7 +192,7 @@ for i in range(numlabel3):
     cv2.waitKey(0)
 
 #for label3(abRabL)
-for i in range(numlabel4):
+for i in range(len(box1_l4)):
     img1 = cv2.imread(path+path_p1_l4 + '/' + box1_l4[i])
     img2 = cv2.imread(path+path_p2_l4 + '/' + box2_l4[i])
     img3 = cv2.imread(path+path_p3_l4 + '/' + box3_l4[i])
